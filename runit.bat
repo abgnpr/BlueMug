7e44dc5b97c49a02c76c11f0ac38fb8cec6e077a
Abhigyan Prakash <ap44122@gmail.com>
2018-11-02 10:19:10 +0530

    Add runit.bat
    Opens the chooser to let the user select a .exe file and then runs it.

---
+++ runit.bat
@@ -0,0 +1,38 @@
+<# : 
+
+title RUN
+:select
+cls
+color 3f
+echo ...please wait for selection window...
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    set "cf=%%~I"
+)
+cls
+title RUN: %cf%
+color 2F
+echo.
+set "bsp= "
+IF NOT " %cf%" == "%bsp%" (  
+echo hooraaah  
+"%cf%"
+echo.
+echo.
+pause
+echo. ) else (
+echo: No files selected. ) 
+echo.
+CHOICE /C YN /M "Run more?"
+IF %errorlevel% EQU 1 ( GOTO select )
+IF %errorlevel% EQU 2 ( GOTO :EOF )
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+$loc=type loc.txt
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = $loc
+$f.Filter = "Executable (*.exe)|*.exe|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $false
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
